9d1ef93d7f4d95f7c9393b56b95c46c7f2437a82
Nik-DevLAB <na8647@srmist.edu.in>
2026-02-17 15:40:51 +0530

    Usecase 3

---
+++ src/Palindrome/usecase3Palindrome.java
@@ -0,0 +1,24 @@
+package Palindrome;
+import java.util.*;
+public class usecase2palindromecheckerapp {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Input text: ");
+        String input= sc.next();
+        char[] ch= input.toCharArray();
+        String reversed= "";
+
+        for(int i=input.length()-1; i>=0; i-- ){
+            reversed += ch[i];
+        }
+
+        if(input.equals(reversed)){
+            System.out.println("Is it a Palindrome? : True");
+        }
+        else {
+            System.out.println("Is it a Palindrome? : False");
+        }
+    }
+
+
+}
